9f6bf9b2e315669b0641003aee1cf9986bf1024a
Mustafa Abbas <73982646+envoydead7@users.noreply.github.com>
2025-08-14 08:01:32 +0000

    operations done

--- 01-basics/03_conversion.js
+++ 01-basics/03_conversion.js
@@ -1,10 +1,10 @@
 let score = "33abc";
 
-console.log(typeof score); // number
+// console.log(typeof score); // number
 
 let valueInNumber = Number(score);
 
-console.log(typeof valueInNumber); // number
+// console.log(typeof valueInNumber); // number
 
 // "33" => 33
 //"33abc" => NaN
@@ -22,5 +22,31 @@ console.log(booleanIsloggedIn); // true
 let someNumber = 33
 
 let StringNumber = String(someNumber);
-console.log(typeof StringNumber); // "33"
+// console.log(typeof StringNumber); // "33"
 
+// *********** Operations *************** //
+
+let value = 3;
+
+let negValue = -value;
+// console.log(negValue); // -3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);   
+// console.log(2%2); // 0
+// console.log(2**2); // 4
+
+let str1 = "hello"
+let str2 = "world";
+let str3 = str1 + " " + str2;
+console.log(str3); // "hello world"
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + 2 + "2"); // "12"
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter); // 101
